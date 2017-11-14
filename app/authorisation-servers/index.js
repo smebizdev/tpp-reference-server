3bff1d0ab9e6d1bb1fa5e10804e2f7265b41595c
robmckinnon <rob@movingflow>
2017-11-14 16:47:31 +0000

    Add updateOpenIdConfigs() function to fetch and store configs
    Fetch and store openIdConfig for each authorisation server without openIdConfig.

--- app/authorisation-servers/index.js
+++ app/authorisation-servers/index.js
@@ -2,8 +2,10 @@ const {
   allAuthorisationServers,
   authorisationServersForClient,
   storeAuthorisationServers,
+  updateOpenIdConfigs,
 } = require('./authorisation-servers');
 
 exports.allAuthorisationServers = allAuthorisationServers;
 exports.authorisationServersForClient = authorisationServersForClient;
 exports.storeAuthorisationServers = storeAuthorisationServers;
+exports.updateOpenIdConfigs = updateOpenIdConfigs;

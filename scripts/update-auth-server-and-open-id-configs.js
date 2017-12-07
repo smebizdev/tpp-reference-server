57f43f76fb192ae0ca9d9f79096715b5acfbed13
Ezo Saleh <ezo@the-experimenters.com>
2017-12-07 16:18:12 +0000

    Added logging and debugging capability.

--- scripts/update-auth-server-and-open-id-configs.js
+++ scripts/update-auth-server-and-open-id-configs.js
@@ -1,13 +1,19 @@
 const path = require('path');
 const dotenv = require('dotenv');
+const debug = require('debug')('debug');
+const log = require('debug')('log');
 
-dotenv.load({ path: path.join(__dirname, '..', '.env') });
+const ENVS = dotenv.load({ path: path.join(__dirname, '..', '.env') });
+debug(`ENVs set: ${JSON.stringify(ENVS.parsed)}`);
 
 const { updateOpenIdConfigs } = require('../app/authorisation-servers');
 const { fetchOBAccountPaymentServiceProviders } = require('../app/ob-directory');
 
 const cacheLatestConfigs = async () => {
+  log('Running fetchOBAccountPaymentServiceProviders');
   await fetchOBAccountPaymentServiceProviders();
+
+  log('Running updateOpenIdConfigs');
   await updateOpenIdConfigs();
 };
 
